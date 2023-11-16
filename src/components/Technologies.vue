94297e49523e5ce8f006a0c4cf6dc1f8d023c413
Josef Hrebec <josefhrebec02@gmail.com>
2023-11-16 21:39:47 +0100

    Add functional button logic

--- src/components/Technologies.vue
+++ src/components/Technologies.vue
@@ -1,28 +1,48 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref, toRefs } from "vue";
 
 interface tech {
   name: string;
   img: string;
 }
 
-const props = defineProps<{ tech: tech[]; len: number }>();
+const props = defineProps<{ tech: tech[] }>();
+const { tech } = toRefs(props);
+const len = tech.value.length;
 
 const amount = ref(0);
+const btnRefs = ref([]);
+
+const hintText = computed(() => {
+  if (amount.value == 0) {
+    return "Click me!";
+  } else if (amount.value < len) {
+    return "Almost there!";
+  } else {
+    return "Good job!";
+  }
+});
 
 function handle(event: Event) {
   if (!(event.target as Element)?.classList.contains("clicked")) {
     (event.target as Element)?.classList.add("clicked");
     amount.value++;
+    if (amount.value % len == 0) {
+      btnRefs.value.forEach((btn: Element) => {
+        btn.classList.remove("clicked");
+      });
+    }
   }
 }
 </script>
 
 <template>
-  <div v-for="item in tech">
-    <button @click="handle">{{ item.name }}</button>
+  <div>
+    <button v-for="item in tech" @click="handle" ref="btnRefs">
+      {{ item.name }}
+    </button>
+    <div>{{ hintText }}</div>
   </div>
-  <div>{{ amount }}</div>
 </template>
 
 <style>
@@ -38,7 +58,6 @@ button {
 
 .clicked {
   background-color: var(--backg-white);
-  /* border: 1px solid var(--text-dark); */
   box-shadow: inset 0px 0.5px 1px 0.5px rgba(0, 0, 0, 0.15);
 }
 </style>
