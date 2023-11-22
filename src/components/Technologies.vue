44b6169a9b32bad52514b8e6afdc368893c74c96
Josef Hrebec <josefhrebec02@gmail.com>
2023-11-22 11:35:31 +0100

    Layout media query and tech transitions

--- src/components/Technologies.vue
+++ src/components/Technologies.vue
@@ -15,11 +15,11 @@ const btnRefs = ref([]);
 
 const hintText = computed(() => {
   if (amount.value == 0) {
-    return "Click me!";
+    return "";
   } else if (amount.value < len) {
-    return "Almost there!";
+    return "blur-one";
   } else {
-    return "Good job!";
+    return "blur-two";
   }
 });
 
@@ -40,41 +40,65 @@ watch(amount, (curr) => {
 </script>
 
 <template>
-  <div>
+  <div class="main-wrap">
     <div class="button-wrap">
       <button v-for="item in tech" @click="handle" ref="btnRefs">
         <img :src="item.img" alt="Tech logo" />
         <div>{{ item.name }}</div>
       </button>
+      <button class="bun">
+        <img :class="hintText" src="/assets/bun.png" alt="Tech logo" />
+        <div :class="hintText">Bun?</div>
+      </button>
     </div>
-    <Transition mode="out-in" name="swap">
-      <button v-if="hintText === 'Click me!'">Edit</button>
-      <button v-else-if="hintText === 'Almost there!'">Save</button>
-      <button v-else-if="hintText === 'Good job!'">Cancel</button>
-    </Transition>
+
+    <!-- <Transition mode="out-in" name="swap">
+      <div class="hint" v-if="hintText === 'Click me!'">Click me!</div>
+      <div class="hint" v-else-if="hintText === 'Almost there!'">
+        Almost there!
+      </div>
+      <div class="hint last" v-else-if="hintText === 'Good job!'">
+        Good job!
+      </div>
+    </Transition> -->
   </div>
 </template>
 
 <style scoped>
 .swap-enter-active,
 .slide-up-leave-active {
-  transition: all 0.25s ease-out;
+  transition: all 0.5s ease-out;
 }
 
 .swap-enter-from {
   opacity: 0;
-  transform: translateY(30px);
+  transform: translateY(40px);
 }
 
 .swap-leave-to {
   opacity: 0;
-  transform: translateY(-30px);
+  transform: translateY(-40px);
+}
+.main-wrap {
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
 }
 .button-wrap {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   gap: 8px;
+}
+
+.hint {
+  font-weight: 700;
+  font-size: 1.8em;
+  margin: 0 auto;
+}
+
+.last {
+  color: gold;
 }
 
 button {
@@ -104,7 +128,28 @@ button > div {
   box-shadow: inset 0px 0.5px 1px 0.5px rgba(0, 0, 0, 0.35);
 }
 
+.bun > * {
+  filter: blur(8px);
+  transition: all 0.5s;
+}
+
+.bun > .blur-one {
+  filter: blur(4px);
+}
+
+.bun > .blur-two {
+  filter: blur(0);
+}
+
 img {
   height: 2.8em;
+  max-height: 100%;
+}
+
+@media screen and (min-width: 992px) {
+  .main-wrap {
+    max-width: 80%;
+    margin: 0 auto;
+  }
 }
 </style>
